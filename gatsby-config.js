68028e6734db89d45724188c742df49ef8b4d4ee
Monknow <juanrodriguezcode@gmail.com>
2021-08-09 13:09:46 -0400

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  siteMetadata: {
+    siteUrl: "https://www.yourdomain.tld",
+    title: "Prueba",
+  },
+  plugins: ["gatsby-plugin-styled-components", "gatsby-plugin-gatsby-cloud"],
+};
