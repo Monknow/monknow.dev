c08d56bd9e52babd6054e33670e772759d20bed8
Monknow <juanrodriguezcode@gmail.com>
2021-09-14 09:26:57 -0400

    Se ha añadido netlify cms

--- gatsby-config.js
+++ gatsby-config.js
@@ -13,6 +13,14 @@ module.exports = {
 				},
 			},
 		},
+		{
+			// keep as first gatsby-source-filesystem plugin for gatsby image support
+			resolve: "gatsby-source-filesystem",
+			options: {
+				path: `${__dirname}/static/img`,
+				name: "uploads",
+			},
+		},
 		`gatsby-transformer-sharp`,
 		"gatsby-plugin-styled-components",
 		"gatsby-plugin-gatsby-cloud",
@@ -23,16 +31,45 @@ module.exports = {
 					"cache-control: public",
 					"cache-control: max-age=31536000",
 					"cache-control: immutable",
-				], // option to add more headers. `Link` headers are transformed by the below criteria
+				],
 			},
 		},
 		`gatsby-transformer-remark`,
+		`gatsby-plugin-netlify-cms`,
 		`gatsby-plugin-react-helmet`,
 		{
 			resolve: `gatsby-source-filesystem`,
 			options: {
 				name: `svg`,
 				path: `${__dirname}/src/assets/svg/`,
+			},
+		},
+		{
+			resolve: "gatsby-transformer-remark",
+			options: {
+				plugins: [
+					{
+						resolve: "gatsby-remark-relative-images",
+						options: {
+							name: "uploads",
+						},
+					},
+					{
+						resolve: "gatsby-remark-images",
+						options: {
+							// It's important to specify the maxWidth (in pixels) of
+							// the content container as this plugin uses this as the
+							// base for generating different widths of each image.
+							maxWidth: 2048,
+						},
+					},
+					{
+						resolve: "gatsby-remark-copy-linked-files",
+						options: {
+							destinationDir: "static",
+						},
+					},
+				],
 			},
 		},
 		{
